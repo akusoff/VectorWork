f84c77f0500e52dbb1206ad638a833ce000327fe
akusoff <kirill.voloshin.1998@gmail.com>
2020-02-27 00:24:22 +0300

    vector was added

---
+++ src/com/company/Vector.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Vector {
+    static int x, y, z;
+
+    public Vector (int x, int y, int z) {
+        Vector.x = x;
+        Vector.y = y;
+        Vector.z = z;
+    }
+}
